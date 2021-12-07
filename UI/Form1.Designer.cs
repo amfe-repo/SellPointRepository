1eee090685b20e7f0c5812ea93d9b81e24c6e891
Akme <you@example.com>
2021-12-07 17:27:19 -0400

    v4

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(125, 177);
+            this.button1.Location = new System.Drawing.Point(220, 170);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(486, 205);
             this.button1.TabIndex = 0;
