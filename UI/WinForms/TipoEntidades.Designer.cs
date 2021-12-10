d5ad73692fd12a416fcdc566958ff81876894a26
Raymond Salvador Suarez Levasseur <raymondsuarez1407@gmail.com>
2021-12-10 18:24:12 -0400

    TipoEntidadesAdded

---
+++ UI/WinForms/TipoEntidades.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace UI.WinForms
+{
+    partial class TipoEntidades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TipoEntidades));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ChF2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TXEsta2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TXComen2 = new System.Windows.Forms.TextBox();
+            this.TXDescp2 = new System.Windows.Forms.TextBox();
+            this.LBDesc = new System.Windows.Forms.Label();
+            this.CBT = new System.Windows.Forms.ComboBox();
+            this.PN2 = new System.Windows.Forms.Panel();
+            this.BTNDel2 = new System.Windows.Forms.Button();
+            this.BTNSearch2 = new System.Windows.Forms.Button();
+            this.BTNRefre2 = new System.Windows.Forms.Button();
+            this.BTAdd2 = new System.Windows.Forms.Button();
+            this.DGV2 = new System.Windows.Forms.DataGridView();
+            this.panel2.SuspendLayout();
+            this.PN2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.ChF2);
+            this.panel2.Location = new System.Drawing.Point(304, 124);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(187, 28);
+            this.panel2.TabIndex = 19;
+            // 
+            // ChF2
+            // 
+            this.ChF2.AutoSize = true;
+            this.ChF2.BackColor = System.Drawing.Color.Transparent;
+            this.ChF2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChF2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ChF2.Location = new System.Drawing.Point(61, 3);
+            this.ChF2.Name = "ChF2";
+            this.ChF2.Size = new System.Drawing.Size(66, 20);
+            this.ChF2.TabIndex = 9;
+            this.ChF2.Text = "Falso";
+            this.ChF2.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(349, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "NoEliminable";
+            // 
+            // TXEsta2
+            // 
+            this.TXEsta2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXEsta2.Location = new System.Drawing.Point(54, 122);
+            this.TXEsta2.Name = "TXEsta2";
+            this.TXEsta2.Size = new System.Drawing.Size(187, 22);
+            this.TXEsta2.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(111, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Estatus";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(349, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Comentario";
+            // 
+            // TXComen2
+            // 
+            this.TXComen2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXComen2.Location = new System.Drawing.Point(305, 62);
+            this.TXComen2.Name = "TXComen2";
+            this.TXComen2.Size = new System.Drawing.Size(187, 22);
+            this.TXComen2.TabIndex = 14;
+            // 
+            // TXDescp2
+            // 
+            this.TXDescp2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXDescp2.Location = new System.Drawing.Point(54, 62);
+            this.TXDescp2.Name = "TXDescp2";
+            this.TXDescp2.Size = new System.Drawing.Size(187, 22);
+            this.TXDescp2.TabIndex = 13;
+            // 
+            // LBDesc
+            // 
+            this.LBDesc.AutoSize = true;
+            this.LBDesc.BackColor = System.Drawing.Color.Transparent;
+            this.LBDesc.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBDesc.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LBDesc.Location = new System.Drawing.Point(95, 43);
+            this.LBDesc.Name = "LBDesc";
+            this.LBDesc.Size = new System.Drawing.Size(94, 16);
+            this.LBDesc.TabIndex = 12;
+            this.LBDesc.Text = "Descripcion";
+            // 
+            // CBT
+            // 
+            this.CBT.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CBT.FormattingEnabled = true;
+            this.CBT.Location = new System.Drawing.Point(177, 175);
+            this.CBT.Name = "CBT";
+            this.CBT.Size = new System.Drawing.Size(213, 24);
+            this.CBT.TabIndex = 20;
+            // 
+            // PN2
+            // 
+            this.PN2.BackColor = System.Drawing.Color.Transparent;
+            this.PN2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PN2.Controls.Add(this.BTNDel2);
+            this.PN2.Controls.Add(this.BTNSearch2);
+            this.PN2.Controls.Add(this.BTNRefre2);
+            this.PN2.Controls.Add(this.BTAdd2);
+            this.PN2.Location = new System.Drawing.Point(573, 31);
+            this.PN2.Name = "PN2";
+            this.PN2.Size = new System.Drawing.Size(189, 373);
+            this.PN2.TabIndex = 21;
+            // 
+            // BTNDel2
+            // 
+            this.BTNDel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BTNDel2.BackgroundImage")));
+            this.BTNDel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTNDel2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNDel2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BTNDel2.Location = new System.Drawing.Point(30, 297);
+            this.BTNDel2.Name = "BTNDel2";
+            this.BTNDel2.Size = new System.Drawing.Size(134, 48);
+            this.BTNDel2.TabIndex = 3;
+            this.BTNDel2.Text = "Borrar";
+            this.BTNDel2.UseVisualStyleBackColor = true;
+            // 
+            // BTNSearch2
+            // 
+            this.BTNSearch2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BTNSearch2.BackgroundImage")));
+            this.BTNSearch2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTNSearch2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNSearch2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BTNSearch2.Location = new System.Drawing.Point(30, 206);
+            this.BTNSearch2.Name = "BTNSearch2";
+            this.BTNSearch2.Size = new System.Drawing.Size(134, 48);
+            this.BTNSearch2.TabIndex = 2;
+            this.BTNSearch2.Text = "Buscar";
+            this.BTNSearch2.UseVisualStyleBackColor = true;
+            // 
+            // BTNRefre2
+            // 
+            this.BTNRefre2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BTNRefre2.BackgroundImage")));
+            this.BTNRefre2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTNRefre2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNRefre2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BTNRefre2.Location = new System.Drawing.Point(30, 115);
+            this.BTNRefre2.Name = "BTNRefre2";
+            this.BTNRefre2.Size = new System.Drawing.Size(134, 48);
+            this.BTNRefre2.TabIndex = 1;
+            this.BTNRefre2.Text = "Modificar";
+            this.BTNRefre2.UseVisualStyleBackColor = true;
+            // 
+            // BTAdd2
+            // 
+            this.BTAdd2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BTAdd2.BackgroundImage")));
+            this.BTAdd2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTAdd2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTAdd2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.BTAdd2.Location = new System.Drawing.Point(30, 25);
+            this.BTAdd2.Name = "BTAdd2";
+            this.BTAdd2.Size = new System.Drawing.Size(134, 48);
+            this.BTAdd2.TabIndex = 0;
+            this.BTAdd2.Text = "Agregar";
+            this.BTAdd2.UseVisualStyleBackColor = true;
+            // 
+            // DGV2
+            // 
+            this.DGV2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV2.Location = new System.Drawing.Point(35, 226);
+            this.DGV2.Name = "DGV2";
+            this.DGV2.Size = new System.Drawing.Size(503, 168);
+            this.DGV2.TabIndex = 22;
+            // 
+            // TipoEntidades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 435);
+            this.Controls.Add(this.DGV2);
+            this.Controls.Add(this.PN2);
+            this.Controls.Add(this.CBT);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TXEsta2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TXComen2);
+            this.Controls.Add(this.TXDescp2);
+            this.Controls.Add(this.LBDesc);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TipoEntidades";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TipoEntidades";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.PN2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox ChF2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TXEsta2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TXComen2;
+        private System.Windows.Forms.TextBox TXDescp2;
+        private System.Windows.Forms.Label LBDesc;
+        private System.Windows.Forms.ComboBox CBT;
+        private System.Windows.Forms.Panel PN2;
+        private System.Windows.Forms.Button BTNDel2;
+        private System.Windows.Forms.Button BTNSearch2;
+        private System.Windows.Forms.Button BTNRefre2;
+        private System.Windows.Forms.Button BTAdd2;
+        private System.Windows.Forms.DataGridView DGV2;
+    }
+}
